b465411549e1de8df817f7126dc66389a207596a
Dennis Le <le.dennis605@gmail.com>
2018-07-08 15:30:31 -0500

    Complete R-2.5.

--- Exercises/Chapter_2/2_7.py
+++ Exercises/Chapter_2/2_7.py
@@ -49,8 +49,64 @@ class Flower:
         """ Set price for Flower. """
         self._price = price
 
+# R-2.5
+class CreditCard:
+    """ A consumer credit card. pg 70. """
+
+    def __init__(self, customer, bank, acnt, limit):
+        """ Create a new credit card instance.
+
+        The initial balance is zero.
+
+        customer    the name of the customer
+        bank        the name of the bank
+        acnt        the acount identifier
+        limit       credit limit
+        """
+
+        self._customer = customer
+        self._bank = bank
+        self._acnt = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        return self._customer
+
+    def get_bank(self):
+        return self._bank
+
+    def get_acnt(self):
+        return self._acnt
+
+    def get_limit(self):
+        return self._limit
+
+    def get_balance(self):
+        return self._balance
+
+    def charge(self, price):
+        """ Charge given price to the card, assuming sufficient credit limit.
+
+        Return True if charge was processed; False if charge was denied.
+        """
+
+        if isinstance(price, int) or isinstance(price, float):
+            if price + self._balance > self._limit:
+                return False
+            else:
+                self._balance += price
+                return True
+        else:
+            raise ValueError('Can only charge with money.')
+
+    def make_payment(self, amount):
+        """ Process customer payment that reducces balance. """
+        self._balance -= amount
+
 
 if __name__ == '__main__':
+    # R-2.4
     redFlower = Flower('red', 12, 4)
 
     # Getters
@@ -65,3 +121,13 @@ if __name__ == '__main__':
     assert(redFlower.get_num_petals() == 24)
     redFlower.set_price(8)
     assert(redFlower.get_price() == 8)
+
+    # R-2.5
+    chase = CreditCard('Bob', 'Chase', '1234 5678 8765 4321', 4000)
+    chase.charge(500.00)
+    chase.charge(500)
+    try:
+        chase.charge('cookies')
+        assert(False)
+    except ValueError:
+        assert(True)
