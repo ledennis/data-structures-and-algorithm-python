bc68ff2f0a624c2c59ba087cda02d16a126b6ce0
Dennis Le <le.dennis605@gmail.com>
2018-07-12 19:32:36 -0500

    Complete C-2.24.

---
+++ Exercises/Chapter_2/Creativity.py
@@ -0,0 +1,50 @@
+# C-2.24
+"""
+    class Customer(object):
+        variables:
+            Name
+            Email Address
+            BankAcc
+            Library
+
+        methods:
+            readBook()
+            viewStore()
+            viewLibrary()
+
+    class Library(object):
+        variables:
+            libraryList[]
+        methods:
+            addBook()
+            removeBook()
+            printLibrary()
+
+    class Store(object):
+        variables:
+            catalog
+        methods:
+            addBook()
+            removeBook()
+            printCatalog(Library)
+            buyBook(Library)
+
+    class Book(object):
+        variables:
+            Title
+            Author
+            Illustrator
+            Publisher
+            ID
+            Genre
+            Content
+        method:
+            printContent()
+
+    class SoldBook(Book):
+        variables:
+            PurchaseDate
+        method:
+            printPurchaseDate
+
+"""
