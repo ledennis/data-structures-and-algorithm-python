807e30e99870a6e3f9850d740701b78ff5462557
Dennis Le <le.dennis605@gmail.com>
2018-07-25 20:27:50 -0500

    Complete R-3.2.

--- Exercises/Chapter_3/Reinforcement.py
+++ Exercises/Chapter_3/Reinforcement.py
@@ -1,3 +1,7 @@
 # R-3.1
 # 8n < 4nlogn < 2n^2 < n^3 < 2^n
 # Linear < nlogn < quadratic < cubic < exponential
+
+# R-3.2
+# A = 8nlogn, B = 2n^2
+# n = 32, A = 8 * 32 * 5 = 1280, B = 2 * 32 * 32 = 2048
