5ade05fd32f5207b4be12e3a60ea495b0d433fab
Dennis Le <le.dennis605@gmail.com>
2018-07-25 20:43:26 -0500

    Complete R-3.4.

--- Exercises/Chapter_3/Reinforcement.py
+++ Exercises/Chapter_3/Reinforcement.py
@@ -9,3 +9,7 @@
 # R-3.3
 # A = 40n^2, B = 2n^3, n >= n0
 # n = 20, A = 40 * 20 * 20 = 16000, B = 2 * 20 * 20 * 20 = 16000
+
+# R-3.4
+# f(n) = n
+# A linear function will grow linearly on either a log-log scale or a standard scale.
