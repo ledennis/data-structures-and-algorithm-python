addea834291ff726804de81a5b21b0ed753e05e0
Dennis Le <le.dennis605@gmail.com>
2018-08-13 20:10:36 -0500

    Complete C-4.10.

--- Exercises/Chapter_4/Creativity.py
+++ Exercises/Chapter_4/Creativity.py
@@ -11,6 +11,15 @@ def minMax(A, mini=None, maxi=None, length=0):
             mini = A[length]
         return minMax(A, mini, maxi, length+1)
 
+# C-4.10
+# Describe recursive algo to compute the integer of the base-2 logarithm by using only addition and integer division.
+def logarithm(n, log=1):
+    if n <= 2:
+        return log
+    else:
+        return logarithm(n//2, log+1)
+
+
 if __name__ == '__main__':
     # C-4.9
     A = [1,2,3,4,5]
@@ -21,3 +30,8 @@ if __name__ == '__main__':
     print(minMax(B))
     print(minMax(C))
     print(minMax(D))
+
+    print(logarithm(2))
+    print(logarithm(4))
+    print(logarithm(8.9))
+    print(logarithm(23.9))
