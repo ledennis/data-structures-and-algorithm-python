6f50f5777f0ea3b9470dbedb9b5c1249008d5ae7
Dennis Le <le.dennis605@gmail.com>
2018-08-13 20:27:40 -0500

    Complete C-4.11.

--- Exercises/Chapter_4/Creativity.py
+++ Exercises/Chapter_4/Creativity.py
@@ -19,6 +19,23 @@ def logarithm(n, log=1):
     else:
         return logarithm(n//2, log+1)
 
+# C-4.11
+def unique3(S, length=0):
+    ''' Running time is O(n^2) since each element in S is compared n times. '''
+    if length == len(S)-1:
+        return unique(S, length)
+    else:
+        if unique(S, length):
+            return unique3(S, length+1)
+        else:
+            return False
+
+def unique(S, el):
+    count = 0
+    for items in S:
+        if items == S[el]:
+            count+=1
+    return count == 1
 
 if __name__ == '__main__':
     # C-4.9
@@ -31,7 +48,20 @@ if __name__ == '__main__':
     print(minMax(C))
     print(minMax(D))
 
+    # C-4.10
     print(logarithm(2))
     print(logarithm(4))
     print(logarithm(8.9))
     print(logarithm(23.9))
+
+    # C-4.11
+    A = [1,2,3,4,5]
+    B = [1,2,2,4,5]
+    C = [1,2,2,2,5]
+    D = [-2,2,3,4,-2]
+    F = [0,-1,2,3,8]
+    print(unique3(A))
+    print(unique3(B))
+    print(unique3(C))
+    print(unique3(D))
+    print(unique3(F))
